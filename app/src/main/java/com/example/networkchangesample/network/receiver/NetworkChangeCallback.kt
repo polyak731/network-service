c2252ad89c222b25bb3b7e15b5139487e655d521
Maxim Poliakov <maxim.poliakov@requestum.com>
2019-12-18 12:33:19 +0200

    Annotations for threading were fixed

--- app/src/main/java/com/example/networkchangesample/network/receiver/NetworkChangeCallback.kt
+++ app/src/main/java/com/example/networkchangesample/network/receiver/NetworkChangeCallback.kt
@@ -3,7 +3,7 @@ package com.example.networkchangesample.network.receiver
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.*
-import androidx.annotation.MainThread
+import androidx.annotation.AnyThread
 import androidx.annotation.RequiresApi
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -15,11 +15,13 @@ class NetworkChangeCallback(private val messenger: Messenger) : ConnectivityMana
 
     private var mainHandler: Handler = Handler(Looper.getMainLooper())
 
+    @AnyThread
     override fun onAvailable(network: Network) {
         mainHandler.removeCallbacks(action)
         messenger.send(Message.obtain(null, NetworkService.MSG_SET_VALUE, true))
     }
 
+    @AnyThread
     override fun onLost(network: Network) {
         //TODO: looks like android issue, when we trying to check network for cellular when it's lost it's actually active.
         mainHandler.removeCallbacks(action)
